bee563f4ba154b4e6917828d7fcafb0e080619d0
samqyz <samqy.zheng@mail.utoronto.ca>
2024-09-09 12:04:46 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -29,6 +29,41 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            
+
+        int j = 1;
+        while (j < 100) {
+
+            j = getJ(j);
+
         }
+
+        }
+    }
+
+    private static int getJ(int j) {
+        if (j % 3 == 0 && j % 5 == 0) {
+
+            System.out.println("Fizz Buzz");
+
+        }
+        else if (j % 3 == 0) {
+
+            System.out.println("Fizz");
+
+        }
+        else if (j % 5 == 0) {
+
+            System.out.println("Buzz");
+
+        }
+        else {
+
+            System.out.println(j);
+
+        }
+
+        j++;
+        return j;
     }
 }
