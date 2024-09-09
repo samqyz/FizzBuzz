feab0b0d395c287245431b6e180e34441e4860b9
samqyz <samqy.zheng@mail.utoronto.ca>
2024-09-09 12:31:49 -0400

    Made a new file called Main which calls FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,31 +5,6 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-            
 
         int j = 1;
         while (j < 100) {
@@ -37,11 +12,9 @@ class FizzBuzz {
             j = getJ(j);
 
         }
-
-        }
     }
 
-    private static int getJ(int j) {
+    public static int getJ(int j) {
         if (j % 3 == 0 && j % 5 == 0) {
 
             System.out.println("Fizz Buzz");
